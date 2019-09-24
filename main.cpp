4e174c94fdf0f982fc580f7d0ec962cef66b691f
JennGriff <jgriffiths0101@gmail.com>
2019-09-24 09:06:25 -0700

    Added loop to enable add items to list array

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
@@ -15,5 +16,17 @@ int main()
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 
+  if (input == "A"){
+    for(int i = 0; i < 5; i++) {
+      cout << "Add : "
+      cin  <<  item;
+      if (item == "Q"){
+        cout << "QUITTER \n";
+        return 0;
+      }
+      list[i] = item;
+      cout << "\n" << item << " added! \n";
+    }
+  }
   return 0;
 }
